3e3bbc3ababa4c71eb1170537acd8fb89f90fe1e
Connor Shea <connor.james.shea@gmail.com>
2016-07-30 09:12:49 -0600

    Add new files.

---
+++ app/controllers/note_templates_controller.rb
@@ -0,0 +1,53 @@
+class Profiles::NoteTemplatesController < Profiles::ApplicationController
+  before_action :authenticate_user!
+  before_action :set_note_template, only: [:edit, :destroy]
+
+  def index
+    @note_templates = current_user.note_templates
+  end
+
+  def new
+    @note_template = NoteTemplate.new
+  end
+
+  def edit
+  end
+
+  def create
+    @note_template = NoteTemplate.new(note_template_params)
+    @note_template.user = current_user
+
+    if @note_template.save
+      redirect_to(
+        profile_note_templates_path,
+        notice: "'#{@note_template.title}' was successfully created."
+      )
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @note_template.update_attributes
+      edit_note_profile_template_path(@note_template,
+        notice: "'#{@note_template.title}' was successfully updated."
+      )
+    end
+  end
+
+  def destroy
+    if @note_template.destroy
+      redirect_to(profile_note_templates_path, notice: "Note Template was successfully destroyed.")
+    end
+  end
+
+  private
+
+  def note_template_params
+    params.require(:note_template).permit(:note, :title)
+  end
+
+  def set_note_template
+    @note_template = current_user.note_templates.find(params["id"])
+  end
+end
