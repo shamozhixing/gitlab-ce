f2dd844e4e1be7abd993eace0e7ed6611ca961fc
Connor Shea <connor.james.shea@gmail.com>
2016-08-05 15:46:28 -0600

    Add/fix tests.

--- spec/controllers/profiles/note_templates_controller_spec.rb
+++ spec/controllers/profiles/note_templates_controller_spec.rb
@@ -15,16 +15,6 @@ describe Profiles::NoteTemplatesController do
     it 'should list the note templates' do
       get :index
 
-      expect(assigns(:note_templates)).to include(note_template)
-    end
-  end
-
-  describe 'GET #edit' do
-    let(:note_template) { create(:note_template) }
-
-    it 'should allow the user to edit the note template' do
-      get :edit, id: note_template.id
-
       expect(response).to be_success
     end
   end
@@ -34,16 +24,6 @@ describe Profiles::NoteTemplatesController do
       expect do
         post :create, note_template: { title: 'Lorem ipsum dolor', note: 'Lorem ipsum dolor' }
       end.to change { user.note_templates.count }.by(1)
-
-      expect(response).to redirect_to(profile_note_templates_path)
-    end
-  end
-
-  describe 'DELETE #destroy' do
-    let(:note_template) { create(:note_template) }
-
-    it 'redirects to profile_note_templates_path' do
-      delete :destroy, id: note_template.id
 
       expect(response).to redirect_to(profile_note_templates_path)
     end
